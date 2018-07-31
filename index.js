4b886448fd57f40181bb63b1e12770680b1eb05e
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-31 01:16:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ function increaseRankBy(n){
 }
 
 function deepestChild(){
-  return document.getElementById('grand-node').getElementsByTagName('div')[3]
+  return document.getElementById('grand-node').querySelectorAll('div')[3]
 }
