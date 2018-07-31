abd07b41b64c8a31f283f73fdb51c2b88dc82ba2
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-31 01:17:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget(){
 }
 
 function increaseRankBy(n){
-  const lis = document.getElementsByTagName('ul.ranked-list li')
+  let lis = document.querySelectorAll('ul.ranked-list li')
   for (let i = 0; i < lis.length; i++) {
     lis[i].innerHTML += n
   }
