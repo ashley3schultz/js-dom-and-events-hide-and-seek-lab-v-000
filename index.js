f303f14f267af8343d63de67952e53c911543991
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-31 00:48:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function getFirstSelector(selector){
 }
 
 function nestedTarget(){
-  return document.getElementById('app').getElementById('nested').querySelector('div.target')
+  return document.getElementById('nested').querySelector('div.target')
 }
 
 function increaseRankBy(n){
-  const list = document.querySelectorAll('ul.ranked-list li')
+  const list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
   for (let i = 0; i < list.length; i++){
     list[i].innerHTML = (i + 1).toString()
   }
